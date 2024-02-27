0a75ac61953e2038f25a46b843fdb669ff775020
joonavel <joonavel@gmail.com>
2024-02-27 14:15:27 +0900

    Create app main

--- log-analysis/main/views.py
+++ log-analysis/main/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello. You are now on index of main.')
+    return render(request, 'main/index.html')
